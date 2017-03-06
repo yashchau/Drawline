ab3642110d1a2e1b8bb054766a14033249aafbdd
yashchau <yashchau1811@gmail.com>
2017-03-07 00:06:53 +0530

    Minor bug fix

--- app/src/main/java/com/bt/drawline/MainActivity.java
+++ app/src/main/java/com/bt/drawline/MainActivity.java
@@ -12,50 +12,50 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-   // EditText xvalue=(EditText)findViewById(R.id.xvalue);
+    // EditText xvalue=(EditText)findViewById(R.id.xvalue);
     //EditText yvalue=(EditText)findViewById(R.id.yvalue);
-    int i,j;
+    int i, j;
     int startx = 700;
     int starty = 2500;
-    int endx = 700 ;
-    int endy = 2000 ;
-/*    Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
-            .getDefaultDisplay().getWidth(), (int) getWindowManager()
-            .getDefaultDisplay().getHeight(), Bitmap.Config.ARGB_8888);
-    Canvas canvas = new Canvas(bitmap);
-    Paint paint = new Paint();
-   // EditText xvalue=(EditText)findViewById(R.id.xvalue);
-    //EditText yvalue=(EditText)findViewById(R.id.yvalue);*/
-
+    int endx = 700;
+    int endy = 2000;
+    /*    Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+                .getDefaultDisplay().getWidth(), (int) getWindowManager()
+                .getDefaultDisplay().getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        EditText xvalue=(EditText)findViewById(R.id.xvalue);
+        EditText yvalue=(EditText)findViewById(R.id.yvalue);*/
+    ImageView drawingImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ImageView drawingImageView = (ImageView) this.findViewById(R.id.DrawingImageView);
         setContentView(R.layout.activity_main);
-        EditText xvalue=(EditText)findViewById(R.id.xvalue);
-        EditText yvalue=(EditText)findViewById(R.id.yvalue);
-        Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
-                .getDefaultDisplay().getWidth(), (int) getWindowManager()
+
+        drawingImageView = (ImageView) this.findViewById(R.id.DrawingImageView);
+
+        EditText xvalue = (EditText) findViewById(R.id.xvalue);
+        EditText yvalue = (EditText) findViewById(R.id.yvalue);
+        Bitmap bitmap = Bitmap.createBitmap(getWindowManager()
+                .getDefaultDisplay().getWidth(), getWindowManager()
                 .getDefaultDisplay().getHeight(), Bitmap.Config.ARGB_8888);
         final Canvas canvas = new Canvas(bitmap);
-        drawingImageView.setImageBitmap(bitmap);
-       final Paint paint = new Paint();
+//
+//
+        final Paint paint = new Paint();
         paint.setColor(Color.rgb(255, 153, 51));
         paint.setStrokeWidth(10);
-        //int startx = 700;
-        //int starty = 2500;
-        //int endx = 700 ;
-        //int endy = 2000 ;
         canvas.drawLine(startx, starty, endx, endy, paint);
         xvalue.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                String is=v.getText().toString();
-                i=Integer.parseInt(is);
-                endx=endx+i;
+                String is = v.getText().toString();
+                i = Integer.parseInt(is);
+                System.out.println("Value: " + i);
+                endx = endx + i;
                 canvas.drawLine(startx, starty, endx, endy, paint);
-                startx=endx;
+                startx = endx;
 
                 return false;
             }
@@ -63,14 +63,15 @@ public class MainActivity extends AppCompatActivity {
         yvalue.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                String is=v.getText().toString();
-                j=Integer.parseInt(is);
-                endy=endy+j;
+                String is = v.getText().toString();
+                j = Integer.parseInt(is);
+                endy = endy - j;
                 canvas.drawLine(startx, starty, endx, endy, paint);
-                starty=endy;
+                starty = endy;
 
                 return false;
             }
         });
+        drawingImageView.setImageBitmap(bitmap);
     }
 }
